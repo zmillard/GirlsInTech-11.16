6a0390bea7af9bfcdab4083f9da15a8d73f45778
Zoe Millard <zmillard@stevens.edu>
2019-11-16 13:15:12 -0500

    removing old qs and adding new ones

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,86 +1,31 @@
 var quizQuestions = [
   {
-      question: "What franchise would you rather play a game from?",
+      question: "Are you starting your career fresh or building up?",
       answers: [
           {
-              type: "Microsoft",
-              content: "Halo"
+              type: "Other",
+              content: "Yes"
           },
           {
-              type: "Nintendo",
-              content: "Pokemon"
-          },
-          {
-              type: "Sony",
-              content: "Uncharted"
+              type: "Other",
+              content: "No"
           }
       ]
   },
   {
-      question: "Which console would you prefer to play with friends?",
+      question: "Are you excited about creating secure systems?",
       answers: [
           {
-              type: "Microsoft",
-              content: "X-Box"
+              type: "CySec",
+              content: "Yes"
           },
           {
-              type: "Nintendo",
-              content: "Nintendo 64"
+              type: "SWE",
+              content: "No"
           },
           {
-              type: "Sony",
-              content: "Playstation 1"
-          }
-      ]
-  },
-  {
-      question: "Which of these racing franchises would you prefer to play a game from?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "Forza"
-          },
-          {
-              type: "Nintendo",
-              content: "Mario Kart"
-          },
-          {
-              type: "Sony",
-              content: "Gran Turismo"
-          }
-      ]
-  },
-  {
-      question: "Which of these games do you think is best?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "BioShock"
-          },
-          {
-              type: "Nintendo",
-              content: "The Legend of Zelda: Ocarina of Time"
-          },
-          {
-              type: "Sony",
-              content: "Final Fantasy VII"
-          }
-      ]
-  },
-  {
-      question: "What console would you prefer to own?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "X-Box One"
-          },
-          {
-              type: "Nintendo",
-              content: "Wii U"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 4"
+              type: "Cloud",
+              content: "Indifferent"
           }
       ]
   }
