103d3ad7354e51c17c8273bc0d6c96177a94b7ca
Zoe Millard <zmillard@stevens.edu>
2019-11-16 11:42:17 -0500

    Removing extraneous code

--- src/App.js
+++ src/App.js
@@ -123,8 +123,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Quiz</h2>
+          <h1>Something here</h1>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
